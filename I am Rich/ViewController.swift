a50222a15c5eeed518f9056c0fa031ea06171d0a
GokulRG <ramakrishnan.go@husky.neu.edu>
2020-12-25 00:49:58 -0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Gokul Rama on 12/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
